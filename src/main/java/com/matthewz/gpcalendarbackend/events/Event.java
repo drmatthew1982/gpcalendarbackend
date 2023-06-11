4a8693b75e7bdfdd359295ba9748386ddf795f52
Matthew Zhu <drmatthew1982@163.com>
2023-06-11 22:23:04 +1200

    client potention bug fix and event pojo created

---
+++ src/main/java/com/matthewz/gpcalendarbackend/events/Event.java
@@ -0,0 +1,57 @@
+package com.matthewz.gpcalendarbackend.events;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.util.Date;
+
+public class Event {
+    @Setter
+    @Getter
+    String id;
+
+    @Setter
+    @Getter
+    String eventcmt;
+
+    @Setter
+    @Getter
+    String client_id;
+
+    @Setter
+    @Getter
+    String createby;
+
+    @Setter
+    @Getter
+    String assignedto;
+
+    @Setter
+    @Getter
+    Date startTime;
+
+    @Setter
+    @Getter
+    Date endTime;
+
+    @Setter
+    @Getter
+    String created_user_id;
+
+    @Setter
+    @Getter
+    Date created_time;
+    @Setter
+    @Getter
+    String modified_user_id;
+
+    @Setter
+    @Getter
+    Date modified_time;
+
+    @Setter
+    @Getter
+    String reportStatus;
+
+}
