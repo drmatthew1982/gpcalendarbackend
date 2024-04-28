5e650d1ee2452a402b24ba9a55c0174260fa0299
drmatthew1982 <drmatthew1982@163.com>
2024-04-28 19:11:32 +1200

    Update for test server version

--- src/main/java/com/matthewz/gpcalendarbackend/GpcalendarbackendApplication.java
+++ src/main/java/com/matthewz/gpcalendarbackend/GpcalendarbackendApplication.java
@@ -5,10 +5,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.boot.autoconfigure.security.servlet.UserDetailsServiceAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
 
 import java.util.TimeZone;
 
 @SpringBootApplication(exclude= {SecurityAutoConfiguration.class})
+@ComponentScan(basePackages = {"com.matthewz"})
 @MapperScan(basePackages = {"com.matthewz.gpcalendarbackend.mapper"})
 public class GpcalendarbackendApplication {
 
